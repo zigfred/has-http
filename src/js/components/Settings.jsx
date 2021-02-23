ac1407552a003eab4dc09ac06edca5431b698918
zigfred <zigfredead@gmail.com>
2021-02-24 00:09:08 +0200

    Settings page - added settings heatTargetTemp and heatLossHourlyByDegree

--- src/js/components/Settings.jsx
+++ src/js/components/Settings.jsx
@@ -1,111 +1,100 @@
 import dataProvider from "../dataProvider";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-// TODO: add defaultRuleInterval=1000
-class Settings extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      collectionInterval: ""
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+function Settings(props) {
+  const [settings, setSettings] = useState({
+    collectionInterval: 0,
+    heatTargetTemp: 0,
+    heatLossHourlyByDegree: 0
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await dataProvider.getSettings();
+      setSettings(result.data);
+    }
+    fetchData();
+  }, []);
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const result = await dataProvider.updateSettings(settings);
+    setSettings(result.data);
   }
 
-  componentDidMount() {
-    dataProvider.getSettings()
-      .then(
-        (result) => {
-          if (result === null) {
-            this.setState({
-              isLoaded: true
-            });
-          } else {
-            const {collectionInterval} = result.data;
-            this.setState({
-              isLoaded: true,
-              collectionInterval: collectionInterval
-            });
-          }
-        }).catch((error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
-  }
-
-  handleInputChange(event) {
+  function handleInputChange(event) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
 
-    this.setState({
+    setSettings({
       [name]: value
     });
   }
 
-  onSubmit(e) {
-    e.preventDefault();
-    dataProvider.updateSettings( {
-      collectionInterval: this.state.collectionInterval
-    })
-      .then(
-        (result) => {
-          if (result === null) {
-            this.setState({
-              isLoaded: true
-            });
-          } else {
-            this.setState({
-              isLoaded: true,
-              collectionInterval: result.collectionInterval
-            });
-          }
-        }).catch((error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    );
-  }
+  const {
+    collectionInterval,
+    heatTargetTemp,
+    heatLossHourlyByDegree
+  } = settings;
 
-  render() {
-    const { error, isLoaded, collectionInterval } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div>
-          <h2>Settings</h2>
+  return(
+    <div>
+      <h2>Settings</h2>
 
-          <form>
-            <div className="form-group row">
-              <label htmlFor="inputCollectionInterval" className="col-sm-2 col-form-label">Collection Interval</label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="inputCollectionInterval"
-                  placeholder="Collection Interval"
-                  name="collectionInterval"
-                  value={collectionInterval}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-            </div>
-            <button type="submit" className="btn btn-primary" onClick={this.onSubmit}>Submit</button>
-          </form>
+      <form>
+        <div className="form-group row">
+          <label htmlFor="inputCollectionInterval" className="col-sm-2 col-form-label">Collection Interval, s</label>
+          <div className="col-sm-10">
+            <input
+              type="number"
+              min="1"
+              max="60"
+              className="form-control"
+              id="inputCollectionInterval"
+              name="collectionInterval"
+              placeholder="Loading..."
+              value={collectionInterval}
+              onChange={handleInputChange}
+            />
+          </div>
         </div>
-      );
-    }
-  }
+        <div className="form-group row">
+          <label htmlFor="inputHeatTargetTemp" className="col-sm-2 col-form-label">Heat Target Temp, &#8451;</label>
+          <div className="col-sm-10">
+            <input
+              type="number"
+              min="5"
+              max="30"
+              className="form-control"
+              id="inputHeatTargetTemp"
+              name="heatTargetTemp"
+              placeholder="Loading..."
+              value={heatTargetTemp}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label htmlFor="inputHeatLossHourlyByDegree" className="col-sm-2 col-form-label">Heat loss hourly by degree, W</label>
+          <div className="col-sm-10">
+            <input
+              type="number"
+              min="100"
+              max="300"
+              className="form-control"
+              id="inputHeatLossHourlyByDegree"
+              name="heatLossHourlyByDegree"
+              placeholder="Loading..."
+              value={heatLossHourlyByDegree}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={onSubmit}>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Settings;
