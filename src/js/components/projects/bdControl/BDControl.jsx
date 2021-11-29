0dc4072add5e6bdf10f8b1b50a9b32f2d83c4109
zigfred <zigfredead@gmail.com>
2021-11-29 20:22:42 +0200

    BD - updated with new controller - removed dist

--- src/js/components/projects/bdControl/BDControl.jsx
+++ src/js/components/projects/bdControl/BDControl.jsx
@@ -30,32 +30,38 @@ export function BDControl (props) {
       <h2>Lower Boiler control</h2>
       <div>
         <p>
-          TA top: {lastData['28ff0579b516038c']}<br/>
-          TA mid: {lastData['28ffcf09b316036a']}<br/>
-          TA bottom: {lastData['28ff74f0b216031c']}<br/>
-          BD out: {lastData['28ff69250117054d']}<br/>
+          TA/BT top: {lastData['28ff0579b516038c']} // {lastData['2802000781490148']}<br/>
+          TA/BT mid: {lastData['28ffcf09b316036a']} // {lastData['281900005906008c']}<br/>
+          TA/BT bot: {lastData['28ff74f0b216031c']} // {lastData['28030000761f008d']}<br/>
+          BD in-out: {lastData['280200079540012e']} => {lastData['280c0107165d0113']}<br/>
           BD case: {lastData['28ff4b5662180392']}<br/>
           TA wall: {lastData['28ff14170117035e']}<br/>
-          FLOW: {lastData['bd-flow']}<br/>
         </p>
         <table className="table table-bordered table-sm">
           <tbody>
             <tr>
               <td>
-                <span className={lastData['bd-heater-run'] ? disabledClass : enabledClass}>Run</span>
+                <span className={lastData['bd-heater-pump'] ? enabledClass : disabledClass}>Pump</span>
               </td>
-              <td><span className={lastData['bd-heater-1'] ? disabledClass : enabledClass}>1</span></td>
-              <td><span className={lastData['bd-heater-2'] ? disabledClass : enabledClass}>2</span></td>
-              <td><span className={lastData['bd-heater-3'] ? disabledClass : enabledClass}>3</span></td>
-              <td><span className={lastData['bd-heater-4'] ? disabledClass : enabledClass}>4</span></td>
-              <td><span className={lastData['bd-heater-5'] ? disabledClass : enabledClass}>5</span></td>
-              <td><span className={lastData['bd-heater-6'] ? disabledClass : enabledClass}>6</span></td>
+              <td>
+                <span className={lastData['bd-heater-run'] ? enabledClass : disabledClass}>Run</span>
+              </td>
+              <td><span className={lastData['bd-heater-1'] ? enabledClass : disabledClass}>1</span></td>
+              <td><span className={lastData['bd-heater-2'] ? enabledClass : disabledClass}>2</span></td>
+              <td><span className={lastData['bd-heater-3'] ? enabledClass : disabledClass}>3</span></td>
+              <td><span className={lastData['bd-heater-4'] ? enabledClass : disabledClass}>4</span></td>
+              <td><span className={lastData['bd-heater-5'] ? enabledClass : disabledClass}>5</span></td>
+              <td><span className={lastData['bd-heater-6'] ? enabledClass : disabledClass}>6</span></td>
             </tr>
           <tr>
-            <td>Flow: {lastData['bd-flow']}</td>
+            <td>{lastData['bd-flow']} pulses</td>
+            <td>{lastData['bd-trans-pump']} A</td>
             <td>{lastData['bd-trans-1']} A</td>
             <td>{lastData['bd-trans-2']} A</td>
             <td>{lastData['bd-trans-3']} A</td>
+            <td>{lastData['bd-trans-4']} A</td>
+            <td>{lastData['bd-trans-5']} A</td>
+            <td>{lastData['bd-trans-6']} A</td>
           </tr>
           </tbody>
         </table>
